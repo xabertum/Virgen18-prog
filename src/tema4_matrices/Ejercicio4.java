5c3db68dd468dc560f2528e211c4d07e7d42044a
xabertum <mohatar@gmail.com>
2018-01-20 20:10:26 +0100

    promedioPAres method added

--- src/tema4_matrices/Ejercicio4.java
+++ src/tema4_matrices/Ejercicio4.java
@@ -8,32 +8,64 @@ package tema4_matrices;
  *
  */
 public class Ejercicio4 {
-    
-    public static void iniciarMatriz (int matriz [][]) {
+
+    public static void iniciarMatriz(int matriz[][]) {
 	int cont = 1;
-	
+
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
 		matriz[i][j] = cont;
-		cont++;		
+		cont++;
 	    }
 	}
     }
+
+    public static int sumarPares(int matriz[][]) {
+
+	int suma = 0;
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+
+		if (matriz[i][j] % 2 == 0) {
+		    suma += matriz[i][j];
+		}
+	    }
+	}	
+	
+	return suma;
+    }
     
     
-    
-    
+    public static int promedioPares(int matriz [][]) {
+		
+	int suma = 0;
+	int cont = 0;
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+				
+		if (matriz[i][j] %2 == 0) {
+		    suma = suma + matriz[i][j];
+		    cont++;		    		    
+		} 
+	    }
+	}
+	
+	return suma/cont;	
+    }
     
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	int matriz[][] = new int [3][3];
+	int matriz[][] = new int[3][3];
+		
 	iniciarMatriz(matriz);
 	
-	
-
+	System.out.println("La suma de los numeros pares es: " + sumarPares(matriz));
+	System.out.println("La media es: " + promedioPares(matriz));
     }
 
 }
