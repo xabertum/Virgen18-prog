300ca44c8f65e8da1eb94834a25181f110139686
xabertum <mohatar@gmail.com>
2018-01-20 14:53:49 +0100

    Method mostrarMatriz added

--- src/tema4_matrices/Ejercicio2.java
+++ src/tema4_matrices/Ejercicio2.java
@@ -9,33 +9,39 @@ package tema4_matrices;
  */
 public class Ejercicio2 {
 
-    
-    static void iniciarMatriz (int matriz[][]) {
-	
+    static void iniciarMatriz(int matriz[][]) {
+
 	int cont = 1;
-	
+
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
 		matriz[i][j] = cont;
 		cont++;
 	    }
-	}	
+	}
+    }
+
+    static void mostrarMatriz(int matriz[][]) {
+
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+
+		System.out.print(matriz[i][j] + "");
+
+	    }
+
+	    System.out.println("");
+	}
     }
     
-    
-    
-    
-    
-    
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-	
-	
-	
-	
 
+	
+	
     }
 
 }
