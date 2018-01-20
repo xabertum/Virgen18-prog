001c2f9e13c2f8dd3391c27b5a8a95425237cda2
xabertum <mohatar@gmail.com>
2018-01-20 14:51:11 +0100

    method inciarMatriz Added

--- src/tema4_matrices/Ejercicio2.java
+++ src/tema4_matrices/Ejercicio2.java
@@ -9,6 +9,24 @@ package tema4_matrices;
  */
 public class Ejercicio2 {
 
+    
+    static void iniciarMatriz (int matriz[][]) {
+	
+	int cont = 1;
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		matriz[i][j] = cont;
+		cont++;
+	    }
+	}	
+    }
+    
+    
+    
+    
+    
+    
     /**
      * @param args
      */
