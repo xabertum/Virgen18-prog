701a3dd812ffe0253472f8f456c7f6c5167433c8
xabertum <mohatar@gmail.com>
2018-01-20 14:46:03 +0100

    Added handle exception

--- src/tema4_matrices/Ejercicio1.java
+++ src/tema4_matrices/Ejercicio1.java
@@ -9,7 +9,7 @@ package tema4_matrices;
  */
 public class Ejercicio1 {
 
-    static void iniciarMatriz(int matriz[][]) {
+    static void iniciarMatriz(int matriz[][]) throws Exception {
 	int cont = 1;
 
 	for (int i = 0; i < matriz.length; i++) {
@@ -21,7 +21,7 @@ public class Ejercicio1 {
 
     }
 
-    static void mostrarMatriz(int matriz[][]) {
+    static void mostrarMatriz(int matriz[][]) throws Exception {
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
 		System.out.print(matriz[i][j] + " ");
@@ -30,7 +30,7 @@ public class Ejercicio1 {
 	}
     }
 
-    static int[][] sumarMatrizes(int matriz1[][], int matriz2[][]) {
+    static int[][] sumarMatrizes(int matriz1[][], int matriz2[][]) throws Exception {
 
 	int matrizSuma[][] = new int[matriz1.length][matriz2.length];
 
@@ -54,10 +54,15 @@ public class Ejercicio1 {
 	int matriz1[][] = new int[3][3];
 	int matriz2[][] = new int[3][3];
 
-	iniciarMatriz(matriz1);
-	iniciarMatriz(matriz2);
-		
-	mostrarMatriz(sumarMatrizes(matriz1, matriz2));
+	try {
+	    iniciarMatriz(matriz1);
+	    iniciarMatriz(matriz2);
+	    mostrarMatriz(sumarMatrizes(matriz1, matriz2));
+
+	} catch (Exception e) {
+	    
+	    System.out.println("Matrices de diferente dimension");
+	}
 
     }
 
