dab5b8b9ce0d5aad482b01a571b40d96a016aa97
xabertum <mohatar@gmail.com>
2018-01-20 19:28:22 +0100

    Added iniciarMAtriz method

--- src/tema4_matrices/Ejercicio4.java
+++ src/tema4_matrices/Ejercicio4.java
@@ -8,12 +8,31 @@ package tema4_matrices;
  *
  */
 public class Ejercicio4 {
+    
+    public static void iniciarMatriz (int matriz [][]) {
+	int cont = 1;
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		matriz[i][j] = cont;
+		cont++;		
+	    }
+	}
+    }
+    
+    
+    
+    
+    
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	int matriz[][] = new int [3][3];
+	iniciarMatriz(matriz);
+	
+	
 
     }
 
