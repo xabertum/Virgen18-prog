684c93587c44462eb1db7d775d2ad02fcb384917
xabertum <mohatar@gmail.com>
2018-01-20 15:37:49 +0100

    Final commit

--- src/tema4_matrices/Ejercicio2.java
+++ src/tema4_matrices/Ejercicio2.java
@@ -1,7 +1,9 @@
 /**
  * 
  */
+
 package tema4_matrices;
+import java.util.Scanner;
 
 /**
  * @author xabertum
@@ -26,7 +28,7 @@ public class Ejercicio2 {
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
 
-		System.out.print(matriz[i][j] + "");
+		System.out.print(matriz[i][j] + " ");
 
 	    }
 
@@ -34,12 +36,38 @@ public class Ejercicio2 {
 	}
     }
     
+    
+    static boolean buscarNumMatriz(int matriz [][], int num) {
+	
+	boolean isTrue = false;
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		
+		if(matriz[i][j] == num) isTrue = true;
+		
+	    }
+	}
+		
+	return isTrue;
+    }
 
     /**
      * @param args
      */
     public static void main(String[] args) {
 
+	Scanner scanner = new Scanner(System.in);
+		
+	int matriz [][] = new int [3][3];
+	
+	iniciarMatriz(matriz);
+	
+	System.out.println("Introduce un numero: ");
+	int num = scanner.nextInt();		
+	
+	System.out.println(buscarNumMatriz(matriz, num));
+	mostrarMatriz(matriz);
 	
 	
     }
