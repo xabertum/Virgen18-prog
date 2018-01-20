147a2ece57624d27586f8d42551e4fe17d808eea
xabertum <mohatar@gmail.com>
2018-01-20 20:22:52 +0100

    improved method sumarPares()

--- src/tema4_matrices/Ejercicio4.java
+++ src/tema4_matrices/Ejercicio4.java
@@ -20,39 +20,24 @@ public class Ejercicio4 {
 	}
     }
 
-    public static int sumarPares(int matriz[][]) {
+    public static int[] sumarPares(int matriz[][]) {
 
-	int suma = 0;
+	int suma[] = new int [2];
+	int cont = 0;
 	
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
 
 		if (matriz[i][j] % 2 == 0) {
-		    suma += matriz[i][j];
+		    suma[0] += matriz[i][j];
+		    cont++;
 		}
 	    }
 	}	
 	
+	suma[1] = suma[0]/cont;
+	
 	return suma;
-    }
-    
-    
-    public static int promedioPares(int matriz [][]) {
-		
-	int suma = 0;
-	int cont = 0;
-	
-	for (int i = 0; i < matriz.length; i++) {
-	    for (int j = 0; j < matriz[i].length; j++) {
-				
-		if (matriz[i][j] %2 == 0) {
-		    suma = suma + matriz[i][j];
-		    cont++;		    		    
-		} 
-	    }
-	}
-	
-	return suma/cont;	
     }
     
 
@@ -61,11 +46,14 @@ public class Ejercicio4 {
      */
     public static void main(String[] args) {
 	int matriz[][] = new int[3][3];
-		
+	int resultado [] = new int [2];	
 	iniciarMatriz(matriz);
 	
-	System.out.println("La suma de los numeros pares es: " + sumarPares(matriz));
-	System.out.println("La media es: " + promedioPares(matriz));
+	resultado = sumarPares(matriz);
+	
+	System.out.println("La suma de los numeros pares es: " + resultado[0]);
+	System.out.println("El promedio de los numeros pares es: " + resultado[1]);
+	
     }
 
 }
