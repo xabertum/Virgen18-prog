85d3042435c0036e97df70bd92c252979439a812
xabertum <mohatar@gmail.com>
2018-01-21 13:02:04 +0100

    Final commit

--- src/tema4_matrices/Ejercicio5.java
+++ src/tema4_matrices/Ejercicio5.java
@@ -31,25 +31,40 @@ public class Ejercicio5 {
 	    System.out.println("");
 	}
     }
-    
-    
-    public static int[] minMaxMatriz(int matriz [][]) {
-	int minMax[] = new int [2];
-	
+
+    public static int[] minMaxMatriz(int matriz[][]) {
+	int minMax[] = new int[2];
+
 	minMax[0] = matriz[0][0];
-	
+
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
-		
-		if (matriz[i][j] > minMax[0]) minMax[1] = matriz[i][j];
+
+		if (matriz[i][j] > minMax[0])
+		    minMax[1] = matriz[i][j];
 		else {
 		    matriz[i][j] = minMax[0];
-		}	
+		}
 	    }
 	}
-	
+
 	return minMax;
-	
+
+    }
+
+    public static int media(int matriz[][]) {
+	int media = 0;
+	int cont = 0;
+
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		media += matriz[i][j];
+		cont++;
+	    }
+	}
+
+	return media / cont;
+
     }
 
     /**
@@ -58,17 +73,16 @@ public class Ejercicio5 {
     public static void main(String[] args) {
 
 	int matriz[][] = new int[3][3];
-	int minMaxMatriz [] = new int [2];
-	
+	int minMaxMatriz[] = new int[2];
+
 	inciarMatriz(matriz);
 	mostrarMatriz(matriz);
-	
+
 	minMaxMatriz = minMaxMatriz(matriz);
 	
 	System.out.println("El maximo es: " + minMaxMatriz[1]);
 	System.out.println("El minimo es: " + minMaxMatriz[0]);
-	
-
+	System.out.println("La media de la matriz es: " + media(matriz));
 
     }
 
