4a66614a7b499c91a1315f46b228d4399a549a8b
xabertum <mohatar@gmail.com>
2018-01-21 13:12:30 +0100

    Initial commit

---
+++ src/tema4_matrices/Ejercicio7.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package tema4_matrices;
+
+/**
+ * @author xabertum
+ *
+ */
+public class Ejercicio7 {
+
+    public static void iniciarMatriz(int matriz[][]) {
+
+	int cont = 1;
+
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		matriz[i][j] = cont;
+		cont++;
+	    }
+	}
+    }
+
+    public static void mostrarMatriz(int matriz[][]) {
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+
+		System.out.print(matriz[i][j] + " ");
+	    }
+
+	    System.out.println("");
+	}
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	int matriz [][] = new int [3][3];
+	
+	iniciarMatriz(matriz);
+	mostrarMatriz(matriz);
+	
+
+    }
+
+}
