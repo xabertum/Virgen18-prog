2c7ad8923f04c5a36d1d30c1124546ee80aefe77
xabertum <mohatar@gmail.com>
2018-01-21 12:12:24 +0100

    Added methods

--- src/tema4_matrices/Ejercicio5.java
+++ src/tema4_matrices/Ejercicio5.java
@@ -3,17 +3,52 @@
  */
 package tema4_matrices;
 
+
 /**
  * @author xabertum
  *
  */
 public class Ejercicio5 {
 
+    public static void inciarMatriz(int matriz [][]) {
+	
+	int cont = 1;
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		
+		matriz[i][j] = cont;
+		cont++;		
+	    }
+	}	
+    }
+    
+    public static void mostrarMatriz(int matriz[][]) {
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		
+		System.out.print(matriz[i][j] + " ");
+		
+	    }
+	    
+	    System.out.println("");
+	}
+    }
+    
+    
+    
+    
+    
+    
     /**
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	int matriz [][] = new int [3][3];
+	
+	inciarMatriz(matriz);
+	mostrarMatriz(matriz);
+	
 
     }
 
