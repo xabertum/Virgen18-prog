8c74cb6604caaf04ebbb9de0ea4b3aa2ce65d10f
xabertum <mohatar@gmail.com>
2018-01-21 01:49:26 +0100

    Minor changes

--- src/tema4_matrices/Ejercicio4.java
+++ src/tema4_matrices/Ejercicio4.java
@@ -9,6 +9,10 @@ package tema4_matrices;
  */
 public class Ejercicio4 {
 
+    /**
+     * 
+     * @param matriz
+     */
     public static void iniciarMatriz(int matriz[][]) {
 	int cont = 1;
 
@@ -65,6 +69,28 @@ public class Ejercicio4 {
 	return suma;
     }
 
+    public static int[] sumaPosicionesPares(int matriz[][]) {
+	int resultado[] = new int[2];
+	int cont = 0;
+
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+
+		if (i % 2 == 0) {
+		    resultado[0] += matriz[i][j];
+		    cont++;
+
+		}
+
+	    }
+	}
+
+	resultado[1] = resultado[0] / cont;
+
+	return resultado;
+
+    }
+
     /**
      * @param args
      */
@@ -73,16 +99,24 @@ public class Ejercicio4 {
 	int matriz[][] = new int[3][3];
 	int resultadoPares[] = new int[2];
 	int resultadoImpares[] = new int[2];
+	int resultadoPosicionPar[] = new int [2];
+	
 
 	iniciarMatriz(matriz);
 	resultadoPares = sumarPares(matriz);
 	resultadoImpares = sumarImpares(matriz);
+	resultadoPosicionPar = sumaPosicionesPares(matriz);
+	
 
 	System.out.println("La suma de los numeros pares es: " + resultadoPares[0]);
 	System.out.println("El promedio de los numeros pares es: " + resultadoPares[1] + "\n");
 
 	System.out.println("La suma de los numeros impares es: " + resultadoImpares[0]);
-	System.out.println("El promedio de los numeros impares es: " + resultadoImpares[1]);
+	System.out.println("El promedio de los numeros impares es: " + resultadoImpares[1] + "\n");
+	
+	System.out.println("La suma de las posiciones pares es: " + resultadoPosicionPar[0]);
+	System.out.println("El promedio de las posiciones pares es: " + resultadoPosicionPar[1] + "\n");
+
 
     }
 
