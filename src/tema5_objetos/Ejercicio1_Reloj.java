f7425b687709555c9ecc65ae674e3649bb09a078
xabertum <mohatar@gmail.com>
2018-01-22 16:03:01 +0100

    Added Methods

--- src/tema5_objetos/Ejercicio1_Reloj.java
+++ src/tema5_objetos/Ejercicio1_Reloj.java
@@ -9,6 +9,35 @@ package tema5_objetos;
  */
 public class Ejercicio1_Reloj {
 
+    private int horas;
+    private int minutos;
+    private int segundos;
+    
+
+    public int getHoras() {
+	return horas;
+    }
+
+    public void setHoras(int horas) {
+	this.horas = horas;
+    }
+
+    public int getMinutos() {
+	return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+	this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+	return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+	this.segundos = segundos;
+    }
+
     /**
      * @param args
      */
