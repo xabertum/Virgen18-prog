bf16fdf1b5305b3bd1d8256fada56a126d74604c
xabertum <mohatar@gmail.com>
2018-01-22 15:33:40 +0100

    Added Tema 5 package

---
+++ src/tema5_objetos/Ejercicio1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tema5_objetos;
+
+/**
+ * @author xabertum
+ *
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
