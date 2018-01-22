0bde71424ab96bc8e80878440dcfbf1172f5ed19
xabertum <mohatar@gmail.com>
2018-01-22 15:04:01 +0100

    Methods added

--- src/tema4_matrices/Ejercicio11.java
+++ src/tema4_matrices/Ejercicio11.java
@@ -9,15 +9,33 @@ package tema4_matrices;
  */
 public class Ejercicio11 {
 
+    public static void inciarMatriz(int matriz[][]) {
+	int cont = 1;
+
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+		matriz[i][j] = cont;
+		cont++;
+	    }
+	}
+    }
+
+    public static void mostrarMatriz(int matriz[][]) {
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz[i].length; j++) {
+
+		System.out.print(matriz[i][j] + " ");
+	    }
+
+	    System.out.println("");
+	}
+
+    }
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-
-	
-	
-	
-	
 
     }
 
