41bcaca8b82b1ab249c26543330ad6d4af1697d0
xabertum <mohatar@gmail.com>
2018-01-22 01:26:52 +0100

    Final commit

--- src/tema4_matrices/Ejercicio9.java
+++ src/tema4_matrices/Ejercicio9.java
@@ -3,7 +3,6 @@
  */
 package tema4_matrices;
 
-
 /**
  * @author xabertum
  *
@@ -22,52 +21,56 @@ public class Ejercicio9 {
 	    }
 	}
     }
-    
-    public static void mostrarMatriz(int matriz [][]) {
+
+    public static void mostrarMatriz(int matriz[][]) {
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
-		
+
 		System.out.print(matriz[i][j] + " ");
-		
+
 	    }
-	    
+
 	    System.out.println("");
 	}
-	
+
     }
-    
-    public static int [][] name(int matriz [][]) {
-	
-	int matrizSuma[][];
-	
+
+    public static int[][] sumarMatriz(int matriz[][]) {
+
+	int matrizSuma[][] = new int[matriz.length + 1][matriz.length + 1];
+	int sumaFila[] = new int[matriz.length];
+	int sumaColum[] = new int[matriz.length];
+
 	for (int i = 0; i < matriz.length; i++) {
 	    for (int j = 0; j < matriz[i].length; j++) {
-		
-		
-		
+
+		matrizSuma[i][j] = matriz[i][j];
+
+		sumaFila[i] += matriz[i][j];
+		sumaColum[i] += matriz[j][i];
+
+		matrizSuma[i][matriz.length] = sumaFila[i];
+		matrizSuma[matriz.length][i] = sumaColum[i];
 	    }
-	    
+
 	}
-	
-	
-	
+
 	return matrizSuma;
-	
-	
+
     }
-    
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	
-	int matriz [][] = new int [3][3];
-	
+
+	int matriz[][] = new int[3][3];
+
 	iniciarMatriz(matriz);
 	mostrarMatriz(matriz);
-	
-	
+
+	System.out.println("");
+	mostrarMatriz(sumarMatriz(matriz));
 
     }
 
