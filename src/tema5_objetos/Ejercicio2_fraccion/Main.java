d47bee50f729250994f2bfe4253d6a5c8a5f3e1a
xabertum <mohatar@gmail.com>
2018-01-26 11:26:06 +0100

    Added Main Class

---
+++ src/tema5_objetos/Ejercicio2_fraccion/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tema5_objetos.Ejercicio2_fraccion;
+
+/**
+ * @author xabertum
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	
+	racional racional_1 = new racional(1,4);
+	racional racional_2 = new racional(1,2);
+		
+	
+	System.out.println(racional.multiplica(racional_1, racional_2));
+	
+
+    }
+
+}
