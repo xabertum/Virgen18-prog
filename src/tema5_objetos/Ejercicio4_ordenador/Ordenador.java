e933aa0c3bf1807cb8570eb3b2e0449857ed2f3b
xabertum <mohatar@gmail.com>
2018-02-11 20:39:04 +0100

    Test commit

--- src/tema5_objetos/Ejercicio4_ordenador/Ordenador.java
+++ src/tema5_objetos/Ejercicio4_ordenador/Ordenador.java
@@ -2,4 +2,55 @@ package tema5_objetos.Ejercicio4_ordenador;
 
 public class Ordenador {
 
+    private int memoriaRAM;
+    private String CPU;
+    private String SO;
+    
+    
+    /*******CONSTRUCTOR******/
+    
+    public Ordenador() {
+	
+	this.memoriaRAM = 5;
+	this.CPU = "Intel dual Core";
+	this.SO = "Windows 7";
+	
+    }
+
+    
+    /********** GETTERS and SETTERS*******/
+    public int getMemoriaRAM() {
+        return memoriaRAM;
+    }
+
+    public void setMemoriaRAM(int memoriaRAM) {
+        this.memoriaRAM = memoriaRAM;
+    }
+
+    public String getCPU() {
+        return CPU;
+    }
+
+    public void setCPU(String cPU) {
+        CPU = cPU;
+    }
+
+    public String getSO() {
+        return SO;
+    }
+
+    public void setSO(String sO) {
+        SO = sO;
+    }   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
